8929db29d017e00bd8bb91c4c3a5ebc46406fe96
au4ob10 <agranchelli27@gmail.com>
2024-04-10 18:36:16 -0400

    added insert data and boiler plate code

--- Toyota-dealer-schema.sql
+++ Toyota-dealer-schema.sql
@@ -12,17 +12,44 @@ car_make varchar(35) NOT NULL,
 car_model varchar(35) NOT NULL,
 min_price double(18,2) Not NULL,
 max_price double(18,2) Not NULL,
-milage    int NOT NULL,
+mileage    int NOT NULL,
 color     varchar(35) NOT NULL,
 dealer_id int NOT NULL,
-title_type varchar(15),
-rating    int
+title_type varchar(15)
 );
 
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2018, 'Toyota', 'Corolla', 12000.00, 15000.00, 30000, 'Blue', 101, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2020, 'Toyota', 'Camry', 25000.00, 30000.00, 0, 'White', 102, 'Reconstructed');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2017, 'Toyota', 'Rav4', 18000.00, 22000.00, 35000, 'Silver', 103, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2019, 'Toyota', 'Highlander', 28000.00, 32000.00, 25000, 'Black', 104, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2021, 'Toyota', 'Sienna', 35000.00, 40000.00, 0, 'Red', 105, 'Reconstructed');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2016, 'Toyota', 'Prius', 15000.00, 18000.00, 40000, 'Green', 106, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2022, 'Toyota', 'Tacoma', 30000.00, 35000.00, 0, 'Gray', 107, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2015, 'Toyota', 'Yaris', 10000.00, 13000.00, 45000, 'Yellow', 108, 'Reconstructed');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2023, 'Toyota', 'Avalon', 40000.00, 45000.00, 0, 'Black', 109, 'Clear');
+
+INSERT INTO car_list (car_year, car_make, car_model, min_price, max_price, mileage, color, dealer_id, title_type) 
+VALUES (2014, 'Toyota', 'Camry', 12000.00, 15000.00, 50000, 'Silver', 110, 'Clear');
 
 
 
-Drop table if exists trade_history;
 CREATE TABLE IF NOT EXISTS trade_history (
 car_id int NOT NULL,
 trade_date date,
@@ -32,7 +59,7 @@ buyer_id int,
 car_detail json    -- maybe include car year, model, price, milage, etc.
 );
 
-Drop table if exists customers;
+
 CREATE TABLE IF NOT EXISTS customers (
 cust_id int NOT NULL,
 car_id int NOT NULL,
@@ -42,7 +69,7 @@ cust_lname varchar(35)
 );
 
 -- we can use search details to calculate search count.
-Drop table if exists search_details;
+
 CREATE TABLE IF NOT EXISTS search_details (
 search_id int NOT NULL,
 car_id int NOT NULL,
--- mysql_connection.py
+++ mysql_connection.py
@@ -17,14 +17,19 @@ def buildConnection():
     connection = mysql.connector.connect ( host='localhost', database='toyota_dealer', user='root', password='root')
     return connection
 
+
 def mysql_conn_func():
     try:
-       connection = buildConnection()
-       cursor = connection.cursor(dictionary=True)
-       
-       
-       ### function logic ### 
-    
+       connection = buildConnection() 
+       cursor = connection.cursor()
+        
+       title_filter_sql = "SELECT car_year, car_make, car_model FROM car_list WHERE title_type = %s"
+        
+       cursor.execute(title_filter_sql, ('clear'))
+        
+       car_titles = cursor.fetchone()
+  
+       return car_titles
     except mysql.connector.Error as err:
         print("MySQL Error: ", err)
     finally:
@@ -34,37 +39,34 @@ def mysql_conn_func():
 
 def kafka_conn_func():
     try:
-        # Kafka configuration
         conf = {
-            'bootstrap.servers': "localhost:9093",  
-        }
-       
+            "bootstrap.servers" : "localhost:9093"
+         }
+        
         producer = Producer(**conf)
-    
-        connection = buildConnection()
+        
+        connection = buildConnection() 
+        cursor = connection.cursor()
+        
         if connection.is_connected():
             db_Info = connection.get_server_info()
             print("Connected to MySQL Server version ", db_Info)
-           
+
             cursor = connection.cursor()
             
-             ### function logic ### 
-             
-             
-        event_json = json.dumps(car_repairs) # serialize the data to an event.
-        connection.commit()
-        producer.produce("toyota_dealer", event_json.encode('utf-8'), callback=acked)
-        
+            event_json = json.dumps(car_db_event) # serialize the data to an event.
+            connection.commit()
+            producer.produce("car_db", event_json.encode('utf-8'), callback=acked)
+            
+            producer.flush()
+            
     except Error as e:
         print("Error while connecting to MySQL", e)
     finally:
         if (connection.is_connected()):
             cursor.close()
             connection.close()
-        
-if __name__ == '__main__':
-    main()
-        
+
             
 
 
